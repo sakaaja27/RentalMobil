71a3ebdace2533216c62ebb35514ab61870ba93f
KingBramZ <appsakabramasta@gmail.com>
2023-10-22 19:39:34 +0700

    teesting

---
+++ src/main/java/project/rentalmobil/RentalMobil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.rentalmobil;
+
+/**
+ *
+ * @author sakab
+ */
+public class RentalMobil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
