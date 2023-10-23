eff26601d29e1feeef25bd6d367c96235320d5c7
KingBramZ <appsakabramasta@gmail.com>
2023-10-23 20:23:36 +0700

    perbaiki bug

---
+++ src/rental/RentalMobil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.rental;
+
+/**
+ *
+ * @author sakab
+ */
+public class RentalMobil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Test");
+    }
+}
