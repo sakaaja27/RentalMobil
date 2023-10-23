1ba13ca90293507dfde779782d08921d621df0fb
Hidayah-Arif <hidayaharif1234@gmail.com>
2023-10-24 04:51:05 +0700

    Fix All

---
+++ build/classes/rental/RentalMobil.rs
@@ -0,0 +1,2 @@
+project.rental.RentalMobil
+RentalMobil
---
+++ src/rental/RentalMobil.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rental;
+
+/**
+ *
+ * @author sakab
+ */
+public class RentalMobil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Test");
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
